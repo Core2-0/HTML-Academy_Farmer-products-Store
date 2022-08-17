98e7c658cbdb60d9c24e906c03bbf2e9e06ebdf9
Roman Chentsov <Core2-00@mail.ru>
2022-08-17 21:14:30 +0500

    add routing

--- src/components/blocks/buy-popup/styles.js
+++ src/components/blocks/buy-popup/styles.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
+import Title from "../../ui/title/title";
 import Button from "../../ui/button/button";
 
 export const Wrapper = styled.div`
   min-width: 500px;
-  padding: 20px;
+  padding: 60px;
   background-color: #fff;
   border: 1px solid rgba(0,0,0,0.1);
 box-shadow: 0px 10px 20px rgba(0,0,0,0.04),
@@ -13,6 +14,11 @@ box-shadow: 0px 10px 20px rgba(0,0,0,0.04),
   top: 30%;
   left: 35%;
   z-index: 100;
+`;
+
+export const PopupTitle = styled(Title)`
+  text-align: center;
+  margin-bottom: 30px;
 `;
 
 export const Close = styled(Button)`
--- src/components/layout/nav/nav.jsx
+++ src/components/layout/nav/nav.jsx
@@ -2,14 +2,37 @@ import React from "react";
 import Logo from "../../ui/logo/logo";
 import Button from "../../ui/button/button";
 import { StyledNav } from "./styles"
+import { AppRoute } from "../../../app-route";
+import { useLocation } from "react-router-dom";
+
+const buttons = [
+  {
+    to: AppRoute.MAIN,
+    button: (
+    <Button minWidth={260} key={AppRoute.MAIN} link={AppRoute.MAIN}>
+        Главная
+    </Button>
+    )
+  },
+  {
+    to: AppRoute.ORDER,
+    button: (
+    <Button minWidth={260} key={AppRoute.ORDER} link={AppRoute.ORDER}>
+        Купить
+    </Button>
+    )
+  },
+];
 
 function Nav() {
+  const pageUrl = useLocation().pathname;
+
   return (
     <StyledNav>
       <Logo />
-      <Button minWidth={260} link="/order">
-        Купить
-      </Button>
+      {buttons
+        .filter((button) => button.to !== pageUrl)
+        .map((button) => button.button)}
     </StyledNav>
   );
 }
